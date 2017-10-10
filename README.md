c6760b8a85c2efd9102dface40d6584f7033295a
Eric Zavesky <ezavesky@research.att.com>
2017-10-10 17:33:04 -0500

    - initial commit of working mood classifier, some
    cleanup is needed

--- README.md
+++ README.md
@@ -1,35 +1,96 @@
-# image-emotion-classification
+# image-mood-classification
 A model example for image classification (for emotional impact).  This model
 relies on a first pass with image classification features and then learns
 a secondary classification layer.
 
-## Image Emotion Classification 
+## Image Mood Classification
+Adapting a task of image mood classification, this model learns a set of
+labels on top of the *image-classifier* output features (some 1000 classification tags).
+The training data comes from the "art photos" from this [academic work](https://dl.acm.org/citation.cfm?id=1873965)
+also posted on the authors' [publication website](http://www.imageemotion.org/):
 
-http://www.imageemotion.org/
+    Jana Machajdik and Allan Hanbury. 2010. *Affective image classification using features inspired by psychology and art theory*. In Proceedings of the 18th ACM international conference on Multimedia (MM '10). ACM, New York, NY, USA, 83-92. DOI: https://doi.org/10.1145/1873951.1873965
 
-We separate the data into a training and test set using K-fold Cross Validation (K = 5).
+Specifically, the example images for this dataset are [available here](http://www.imageemotion.org/testImages_artphoto.zip).
+A handful of these images is also available in the [samples](data/samples) directory.
+*Note: These sample images are copyright of their original authors and are provided
+only for testing and demonstration purposes, and are not authorized for sale or redistribution
+outside of this context.*.
+
+Following original experimental definitions, the model is evaluated by
+separating the data into a training and test set using K-fold Cross Validation (K = 5).
+Original performance metrics comparing methods on this data are shown
+below, as found in the [original publication](https://dl.acm.org/citation.cfm?id=1873965).
+
 
 ### Usage
 This package contains runable scripts for command-line evaluation,
 packaging of a model (both dump and posting), and simple web-test
-uses.   All functionality is encapsulsted in the `classify_image.py`
+uses.   All functionality is encapsulated in the `classify_image.py`
 script and has the following arguments.
 
+
 ### Examples
-Example for evaluation of a test image with top 5 results.
+Example for training the classifier on the provided dataset. Note, the
+features have already been processed by the *image-classification (v0.3)*
+model and stored in that native format in [features_testImages_artphoto.csv.bz2](data/features_testImages_artphoto.csv.bz2).
 ```
-/bin/run_local.sh -m model.h5 -i data/model-t.jpg -f keras -l data/keras_class_names.txt -n 5
+python classify_image.py -l data/labels_testImages_artphoto.txt  -i data/features_testImages_artphoto.csv.bz2 -d model
 ```
 
+Example for evaluating a set of features from the *image-classification*
+model.
+```
+python classify_image.py  -i data/example_awe_1.csv -d model -p data/example_mood.csv
+```
 
-### Usage
-*This use case is currently omitted and the image classification
-utility is provided by a keras-build tensorflow model. However, the
-lessons for creating a hybrid keras model are applicable to the tensorflow
-patterns.*
+### Performance Analysis
+A training analysis of results demonstrates that this problem is not trivial.
+Contrary to the results in the original publication, F1 scores for
+methods in this model are not that high.
+```
+(Support Vector Multiclass - linear kernel)
+             precision    recall  f1-score   support
+
+  amusement       0.22      0.23      0.22        22
+      anger       0.07      0.10      0.08        10
+        awe       0.22      0.21      0.22        19
+contentment       0.21      0.27      0.24        11
+    disgust       0.12      0.18      0.14        11
+ excitement       0.48      0.42      0.45        26
+       fear       0.44      0.50      0.47        22
+        sad       0.30      0.20      0.24        41
+
+avg / total       0.29      0.28      0.28       162
+
+(Random Forest - 300 estimators)
+             precision    recall  f1-score   support
+
+  amusement       0.29      0.27      0.28        22
+      anger       0.00      0.00      0.00        10
+        awe       0.44      0.37      0.40        19
+contentment       0.50      0.36      0.42        11
+    disgust       0.38      0.27      0.32        11
+ excitement       0.50      0.35      0.41        26
+       fear       0.38      0.68      0.49        22
+        sad       0.35      0.41      0.38        41
+
+avg / total       0.37      0.38      0.36       162
+
+```
+
 
 # Example Interface
 An instance should first be built and downloaded from Cognita and then
 launched locally.  Afterwards, the sample application found in 
 [web_demo](web_demo) uses a `localhost` service to classify
 and visualize the results of image classification.
+
+### Example Images
+For the purpose of testing the classifier a few sample images are provided.
+While these images are [approved for commercial use](https://creativecommons.org/licenses/by-nd/2.0/),
+the original author retains all rights.
+
+* ![happy 1](data/example_happy_1.jpg) - [flickr source](https://flic.kr/p/73ZzcE)
+* ![awe 1](data/example_awe_1.jpg) - [flickr source](https://flic.kr/p/RLzkvA)
+* ![excitement 1](data/example_excitement_1.jpg) - [flickr source](https://flic.kr/p/fN8y4d)
---
+++ prediction_formatter.py
@@ -0,0 +1,155 @@
+#! python
+# -*- coding: utf-8 -*-
+"""
+Simple scikit-based transformer for transforming numpy predictions into a dataframe with classes
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.preprocessing import LabelBinarizer
+
+
+class Formatter(BaseEstimator, ClassifierMixin):
+    """Format predictions by binding to class names"""
+    COL_NAME_IDX = "idx"
+    COL_NAME_CLASS = "moods"
+    COL_NAME_PREDICTION = "predictions"
+
+    def __init__(self, class_map=None, classifier=None, class_encoder=None, input_columns=None, input_map=None):
+        """
+        Initialize the formatter with a class map
+        :param class_map: map of {index:string , ...}
+        """
+        self.class_map = class_map
+        self.classifier = classifier
+        self.class_encoder = class_encoder
+        self.input_columns = input_columns
+        self.input_map = input_map
+        self.class_list = None
+        print("SET: {:}".format(self.class_map))
+
+    def learn_class_mapping(self, raw_labels):
+        """Method to learn input mapping from raw samples """
+        # this binarization is required for keras models, disabled for now
+        # https://stackoverflow.com/a/45365714
+        if self.class_encoder is None:
+            self.class_encoder = LabelBinarizer()
+        rawLabel = self.class_encoder.fit_transform(raw_labels)
+        self.class_map = dict(zip(range(len(self.class_encoder.classes_)), self.class_encoder.classes_))
+        return rawLabel
+
+    def learn_input_mapping(self, raw_sample, class_column="classes", group_column="idx", data_column="predictions"):
+        """Method to learn class mapping from raw samples """
+        self.input_columns = {'class':class_column, 'group':group_column, 'data':data_column}
+        classes = raw_sample[self.input_columns['class']].unique().tolist()
+        self.input_map = dict(zip(classes, range(len(classes))))
+
+    def transform_raw_sample(self, raw_sample, raw_labels=None):
+        """Method to transform raw sample into numpy matrix, according to internal class mapping"""
+        if self.input_map is None: return None
+        groupSet = raw_sample.groupby(self.input_columns['group'])
+        numImages = len(groupSet)
+        npData = np.zeros((numImages, len(self.input_map)))
+        listFrames = []
+        listLabels = []
+        for nameG, rowsG in groupSet:
+            idx = len(listFrames)
+            listFrames.append(nameG)
+            if raw_labels is not None:
+                listLabels.append(raw_labels[idx])
+            # https://stackoverflow.com/questions/7837722/what-is-the-most-efficient-way-to-loop-through-dataframes-with-pandas/34311080#34311080
+            for rowDf in zip(rowsG[self.input_columns['class']], rowsG[self.input_columns['data']]):
+                npData[idx][self.input_map[rowDf[0]]] = rowDf[1]
+
+        # if input was a ndarray set, merge them back
+        if listLabels and type(listLabels[0])==np.ndarray:
+            listLabels = np.vstack(listLabels)
+        return {'frames':listFrames, 'values':npData, 'labels':listLabels}
+
+
+    def get_params(self, deep=False):
+        return {'class_map': self.class_map, "classifier": self.classifier, 'class_encoder':self.class_encoder,
+                'input_columns': self.input_columns, 'input_map':self.input_map }
+
+    @property
+    def output_types_(self):
+        _types = self.classes_
+        return [{Formatter.COL_NAME_IDX: _types[0]}, {Formatter.COL_NAME_CLASS: _types[1]}, {Formatter.COL_NAME_PREDICTION: _types[2]}]
+
+    @property
+    def n_outputs_(self):
+        return 3
+
+    @property
+    def classes_(self):
+        return [int, str, float]
+
+    def fit(self, x, y=None):
+        # no real training here, but if we had a classifier, retrain it
+        if self.classifier is not None:
+            self.classifier.fit(x, y)
+        return self
+
+    def predict(self, X, y=None):
+        if type(X)!=pd.DataFrame:
+            print("Error: Input type is not dataframe, aborting!")
+            return None
+        if self.classifier is None:
+            print("Error: No underlying classifier provided, aborting!")
+            return None
+        objTransform = self.transform_raw_sample(X, y)
+        X = np.array(self.classifier.predict_proba(objTransform['values']))
+        print("PREDICT: {:}".format(self.class_map))
+
+        df_predict_set = None
+        for image_idx in range(len(X)):
+            np_predict = X[image_idx,:]
+            print(np_predict)
+            if self.class_list is None:  # may need to init from map one time
+                num_class = len(np_predict)
+                self.class_list = Formatter.prediction_list_gen(self.class_map, range(num_class))
+
+            df_predict = Formatter.prediction_transform(np_predict, self.class_list)
+            df_predict.insert(0, Formatter.COL_NAME_IDX, image_idx)
+
+            if df_predict_set is None:
+                df_predict_set = df_predict
+            else:
+                df_predict_set = df_predict_set.append(df_predict, ignore_index=True)
+        return df_predict_set.reset_index(drop=True)
+
+    def score(self, X, y=None):
+        return 0
+
+    @staticmethod
+    def prediction_list_gen(dict_classes, list_idx):
+        if dict_classes is None:
+            return ['{:}_{:}'.format(Formatter.COL_NAME_CLASS, ix) for ix in list_idx]
+        return [dict_classes[ix] for ix in list_idx]  # NOTE: special -1 offset
+
+    @staticmethod
+    def prediction_transform(preds, class_list=None, path_class=None):
+        """
+        Transform predictions by pairing with class labels
+        :param preds: the numpy result after prediction
+        :param class_list: class list for pairing
+        :param path_class: path for class listing file
+        :return: dataframe sorted by descending probability
+        """
+        df = pd.DataFrame(preds.transpose(), columns=[Formatter.COL_NAME_PREDICTION])
+        if class_list is None:  # must simulate or load the class list
+            print("Formatter: Warning, class map is not currently set...")
+            dict_classes = None
+            if path_class is None or not path_class:
+                dict_classes = eval(open(path_class, 'r').read())
+            print("CONFIRM: {:}".format(list(df.index)))
+            class_list = Formatter.prediction_list_gen(dict_classes, list(df.index))
+        df.insert(0, Formatter.COL_NAME_CLASS, class_list)
+
+        #print("Class is: " + classes[np.argmax(preds) - 1])
+        df.sort_values([Formatter.COL_NAME_PREDICTION], ascending=False, inplace=True)
+        return df
+
