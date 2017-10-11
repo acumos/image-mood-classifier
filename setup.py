0b57d3e53b55bb9d156de3549b164c30a005e4f4
Eric Zavesky <ezavesky@research.att.com>
2017-10-11 05:16:10 -0500

    - start creating package infrastructure

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+# extract __version__ from version file. importing will lead to install failures
+setup_dir = os.path.dirname(__file__)
+with open(os.path.join(setup_dir, 'image_mood_classifier', '_version.py')) as file:
+    globals_dict = dict()
+    exec(file.read(), globals_dict)
+    __version__ = globals_dict['__version__']
+
+
+setup(
+    name = "image_mood_classifier",
+    version = __version__,
+    packages = find_packages(),
+    author = "Eric Zavesky",
+    author_email = "ezavesky@research.att.com",
+    description = ("Image mood classification tool using underlying scene classification"),
+    long_description = ("Image mood classification tool using underlying scene classification"),
+    license = "Apache",
+    package_data={'image_mood_classification':['data/*']},
+    scripts=['bin/run_image-mood-classifier_reference.py'],
+    setup_requires=['pytest-runner'],
+    entry_points="""
+    [console_scripts]
+    """,
+    #setup_requires=['pytest-runner'],
+    install_requires=['cognita_client',
+                      'numpy',
+                      'sklearn',
+                      'image_mood_classifier'],
+    tests_require=['pytest',
+                   'pexpect'],
+    include_package_data=True,
+    )
